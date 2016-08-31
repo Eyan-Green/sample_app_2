afddd0fa50f2df51f95a2668d5c541aca025ab96
Eyan-Green <ian.green736@btinternet.com>
2016-08-31 09:57:45 +0100

    changes root to home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_contact_url
     assert_response :success
     assert_select  "title", "Contact | Ruby on Rails Tutorial Sample App" 
+  end 
 end
